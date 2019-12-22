6e96cb2cfd1ede865e384d22134b4bd86b8bbb1f
adityasunny1189 <adityapathak1189@gmail.com>
2019-12-22 21:22:24 +0530

    functions

---
+++ functions.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+
+using namespace std;
+
+// int fibonacci(int n) {
+//   if(n == 0 || n == 1)
+//     return 1;
+//   else
+//     return (fibonacci(n - 2) + fibonacci(n - 1));
+// }
+//
+// int factorial(int n) {
+//   if(n == 0 || n == 1)
+//     return 1;
+//   else
+//     return (n * factorial(n - 1));
+// }
+
+int main() {
+  int n;
+  cin>>n;
+  cout<<"Entered no: "<<n<<endl;
+  // cout<< "Your name is: " << argv[1]<<endl;
+  // cout<< "factorial of n is: " << factorial(n)<<endl;
+  // cout<< "fibonaci of n is : " << fibonacci(n)<<endl;
+  return 0;
+}
