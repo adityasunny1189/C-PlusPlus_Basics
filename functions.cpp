e09586721f2c049bfe5ae728598e5c21422ee770
adityasunny1189 <adityapathak1189@gmail.com>
2019-12-22 21:28:35 +0530

    functions

--- functions.cpp
+++ functions.cpp
@@ -1,28 +1,29 @@
 #include<iostream>
-
+#include<string>
+#include<vector>
+#include<sstream>
 
 using namespace std;
 
-// int fibonacci(int n) {
-//   if(n == 0 || n == 1)
-//     return 1;
-//   else
-//     return (fibonacci(n - 2) + fibonacci(n - 1));
-// }
-//
-// int factorial(int n) {
-//   if(n == 0 || n == 1)
-//     return 1;
-//   else
-//     return (n * factorial(n - 1));
-// }
+int fibonacci(int n) {
+  if(n == 0 || n == 1)
+    return 1;
+  else
+    return (fibonacci(n - 2) + fibonacci(n - 1));
+}
 
+int factorial(int n) {
+  if(n == 0 || n == 1)
+    return 1;
+  else
+    return (n * factorial(n - 1));
+}
 int main() {
   int n;
+  cout<< "Enter value of n: "<<endl;
   cin>>n;
-  cout<<"Entered no: "<<n<<endl;
   // cout<< "Your name is: " << argv[1]<<endl;
-  // cout<< "factorial of n is: " << factorial(n)<<endl;
-  // cout<< "fibonaci of n is : " << fibonacci(n)<<endl;
+  cout<< "factorial of n is: " << factorial(n)<<endl;
+  cout<< "fibonaci of n is : " << fibonacci(n)<<endl;
   return 0;
 }
