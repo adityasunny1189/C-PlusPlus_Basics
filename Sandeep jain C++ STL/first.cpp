7b4a1255a55a422bf843042d5a0d63534d1454b6
adityasunny1189 <adityapathak1189@gmail.com>
2019-12-24 15:11:19 +0530

    second lecture

--- Sandeep jain C++ STL/first.cpp
+++ Sandeep jain C++ STL/first.cpp
@@ -2,11 +2,21 @@
 using namespace std;
 int main()
 {
-    int x,y;
-    cout << "Enter x: " << endl;
-    cin >> x;
-    cout << "Enter y: " << endl;
-    cin >> y;
-    cout << "Multiplication is: " << (x * y);
+
+  cout << sizeof(char) << endl;
+  cout << sizeof(unsigned char) << endl;
+  cout << sizeof(int) << endl;
+  cout << sizeof(long int) << endl;
+  cout << sizeof(long long int) << endl;
+  cout << sizeof(bool) << endl;
+  cout << sizeof(float) << endl;
+  cout << sizeof(double) << endl;
+
+    // int x,y;
+    // cout << "Enter x: " << endl;
+    // cin >> x;
+    // cout << "Enter y: " << endl;
+    // cin >> y;
+    // cout << "Multiplication is: " << (x * y);
     return 0;
 }
