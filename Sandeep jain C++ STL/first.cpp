b117d6dfc3d02a00c800d102fe170daaa3c89fb2
adityasunny1189 <adityapathak1189@gmail.com>
2019-12-24 14:43:49 +0530

    sandeep jain lectures

---
+++ Sandeep jain C++ STL/first.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout << "Enter x: " << endl;
+    cin >> x;
+    cout << "Enter y: " << endl;
+    cin >> y;
+    cout << "Multiplication is: " << (x * y);
+    return 0;
+}
