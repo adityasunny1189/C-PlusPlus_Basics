9485a84cee303c6ee083c48637cd8f9b9edbed2e
adityasunny1189 <adityapathak1189@gmail.com>
2019-12-31 05:40:42 +0530

    gfg

---
+++ Sandeep jain C++ STL/naiveSearch.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  string source, toSearch;
+  cout << "Enter the source string: "l
+  cin >> source;
+  cout << "Enter the string to search: ";
+  cin >> toSearch;
+  cout << source.length() << endl;
+  // for(int i = 0; i < source.lenght(); i++)
+  return 0;
+}
