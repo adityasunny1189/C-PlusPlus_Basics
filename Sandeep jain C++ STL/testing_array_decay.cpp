7eeefc1eeb40c7cf3216a7cf4df8376198362b99
adityasunny1189 <adityapathak1189@gmail.com>
2020-01-08 19:17:42 +0530

    gfg

--- Sandeep jain C++ STL/testing_array_decay.cpp
+++ Sandeep jain C++ STL/testing_array_decay.cpp
@@ -11,9 +11,24 @@ int addArray(int arr[]) {
   return sum;
 }
 
+int generalAdd(int arr[], int size) {
+  int sum = 0;
+  cout << "size in general function: " << sizeof(arr) << endl;
+  for(int i = 0; i < size; i++)
+    sum += arr[i];
+  return sum;
+}
+
 int main() {
   int arr[5] = {1,1,2,3,5};
   cout << sizeof(arr) << endl;
   cout << addArray(arr) << endl;
+  cout << generalAdd(arr, 5) << endl;
   return 0;
 }
+
+//in array decay the identity of the array is
+//lost and this occurs because of passing of
+//array to a function as arr[1] is a pointer
+//which points to the first memory location
+//which is of integer type.
