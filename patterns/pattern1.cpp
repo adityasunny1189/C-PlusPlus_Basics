8ff4676655a7289da1e3f6d179cb12da137db785
adityasunny1189 <adityapathak1189@gmail.com>
2020-02-07 23:07:37 +0530

    patterns

--- patterns/pattern1.cpp
+++ patterns/pattern1.cpp
@@ -93,21 +93,46 @@
 // 345
 // 4567
 
+// #include<iostream>
+// using namespace std;
+// int main() {
+// 	int n;
+// 	cin >> n;
+// 	int i = 1;
+// 	while(i <= n) {
+// 		int j = 1;
+// 		int count = i;
+// 		while(j <= i) {
+// 			cout << count;
+// 			count++;
+// 			j++;
+// 		}
+// 		cout << endl;
+// 		i++;
+// 	}
+// }
+
+
+// 1
+// 23
+// 456
+// 78910
+
 #include<iostream>
 using namespace std;
 int main() {
 	int n;
 	cin >> n;
 	int i = 1;
+	int count = 1;
 	while(i <= n) {
 		int j = 1;
-		int count = i;
 		while(j <= i) {
 			cout << count;
 			count++;
 			j++;
 		}
-		cout << endl;
 		i++;
+		cout << endl;
 	}
 }
