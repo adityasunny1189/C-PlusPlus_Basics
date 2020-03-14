08a7e189e9c2e8c79ea123828cc8f1eb178c3ac9
adityasunny1189 <adityapathak1189@gmail.com>
2020-03-14 14:29:26 +0530

    sub string

--- subStrings.cpp
+++ subStrings.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 void subString(char str[]) {
 	for(int i = 0; str[i] != '\0'; i++) {
-		for(int j = 0; j <= i; j++) {
-			cout << str[j];
+		for(int j = i; str[j] != '\0'; j++) {
+			for(int k = i; k <= j; k++) {
+				cout << str[k];
+			}
+			cout << endl;
 		}
-		cout << endl;
 	}
 }
 int main() {
