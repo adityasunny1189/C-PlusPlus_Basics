1874e7bfbe306a45374f3a1e9f442f489b44f69a
adityasunny1189 <adityapathak1189@gmail.com>
2020-03-17 22:29:12 +0530

    oops

---
+++ max_char_in_string2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+char maxChar(char input[]) {
+	int freq[256] = {0};
+	for(int i = 0; input[i] != '\0'; i++) {
+
+	}
+}
+int main() {
+	char input[1000];
+	cin >> input;
+	cout << maxChar(input) << endl;
+}
+
+
+//program not completed in development phase
