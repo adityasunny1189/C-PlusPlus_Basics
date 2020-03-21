fd44ac2b4608337fe5d82222b33f6e157a9d5816
adityasunny1189 <adityapathak1189@gmail.com>
2020-03-21 15:37:44 +0530

    recursion

--- codingNinjas/Recursion/first_index_of_a_no.cpp
+++ codingNinjas/Recursion/first_index_of_a_no.cpp
@@ -16,7 +16,7 @@ int index(int arr[], int size, int x) {
 int main() {
 	int size;
 	cin >> size;
-	int *arr  new int[size];
+	int *arr = new int[size];
 	for(int i = 0; i < size; i++) {
 		cin >> arr[i];
 	}
