795913c817dd1691d055c9b2c40e3848002a9bb5
adityasunny1189 <adityapathak1189@gmail.com>
2020-07-24 12:14:16 +0530

    stair case

--- codingNinjas/DynamicProgramming/stairCase.cpp
+++ codingNinjas/DynamicProgramming/stairCase.cpp
@@ -18,6 +18,35 @@ int StairCase(int n) {
 	return oneStep + twoStep + threeStep;
 }
 
+// Memoization / Top Down Approach
+int helper(int n, int *arr) {
+	if(n <= 1) {
+		return 1;
+	}
+	if(arr[n] != -1) {
+		return arr[n];
+	}
+	int oneStep = helper(n - 1, arr);
+	int twoStep = 0;
+	int threeStep = 0;
+	if(n >= 2) {
+		twoStep = helper(n - 2, arr);
+	}
+	if(n >= 3) { 
+		threeStep = helper(n - 3, arr);
+	}
+	arr[n] = oneStep + twoStep + threeStep;
+	return arr[n];
+}
+
+int StairCaseMemo(int n) {
+	int *arr = new int[n + 1];
+	for(int i = 0; i <= n; i++) {
+		arr[i] = -1;
+	}
+	return helper(n, arr);
+}
+
 int main() {
 	freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
@@ -26,6 +55,6 @@ int main() {
 	while(test--) {
 		int n;
 		cin >> n;
-		cout << StairCase(n) << endl;
+		cout << StairCaseMemo(n) << endl;
 	}
 }
