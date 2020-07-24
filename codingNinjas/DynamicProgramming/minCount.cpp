44727807d2f5a077211c4a50149eaa1abdade742
adityasunny1189 <adityapathak1189@gmail.com>
2020-07-24 13:15:10 +0530

    min count

--- codingNinjas/DynamicProgramming/minCount.cpp
+++ codingNinjas/DynamicProgramming/minCount.cpp
@@ -6,12 +6,41 @@ int minCount(int n) {
 	if(n <= 1) {
 		return 0;
 	}
-	stack<int> st;
+	priority_queue<int, vector<int>, greater<int>> pq;
 	for(int i = 1; i <= sqrt(n); i++) {
-		st.push(minCount(n - (i * i)));
+		pq.push(minCount(n - (i * i)));
 	}
-	return st.top() + 1;
+	return pq.top() + 1;
 }
+
+// Memoization / Top down approach
+int helper(int n, int *ans) {
+    if(n < 1) {
+		return 0;
+	}
+	if(n == 1) {
+		return 1;
+	}
+    if(ans[n] != -1) {
+        return ans[n];
+    }
+	priority_queue<int, vector<int>, greater<int>> pq;
+	for(int i = 1; i <= sqrt(n); i++) {
+		pq.push(helper((n - (i * i)), ans));
+	}
+	ans[n] = pq.top() + 1;
+    return ans[n];
+}
+
+int minCountMemo(int n)
+{
+    int *ans = new int[n + 1];
+    for(int i = 0; i < n + 1; i++) {
+        ans[i] = -1;
+    }
+    return helper(n, ans);
+}
+
 
 int main() {
 	freopen("input.txt", "r", stdin);
@@ -21,6 +50,6 @@ int main() {
 	while(test--) {
 		int n;
 		cin >> n;
-		cout << minCount(n) << endl;
+		cout << minCountMemo(n) << endl;
 	}
 }
