6e14e9c0fd5c3c96224db658f808e887eb13fc5e
adityasunny1189 <adityapathak1189@gmail.com>
2020-08-08 20:40:56 +0530

    codeched august

---
+++ codechef--Aug/fourth.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+void skmp(string s, string p) {
+    map<char, int> ourmap;
+    sort(s.begin(), s.end());
+    for(int i = 0; i < s.size(); i++) {
+        if(ourmap.count(s[i]) > 0) {
+            ourmap[s[i]]++;
+            continue;
+        }
+        ourmap[s[i]] = 1;
+    }
+    for(int i = 0; i < p.size(); i++) {
+        ourmap[p[i]]--;
+    }
+    bool added = false;
+    for(auto i = ourmap.begin(); i != ourmap.end(); i++) {
+        if(i->second) {
+            if(!added && (int(i->first) > int(p[0]))) {
+                cout << p;
+                added = true;
+            }
+            for(int j = 0; j < i->second; j++)
+                cout << i->first;
+        }
+    }
+    cout << endl;
+}
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    while(t--) {
+        string s, p;
+        cin >> s >> p;
+        skmp(s, p);
+    }
+}
